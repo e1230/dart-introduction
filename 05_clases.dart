16c6b84c48184f47e2e22cff3263daa4c76a4f55
E1230 <embeltrans@correo.udistrital.edu.co>
2023-05-05 18:52:45 -0500

    name constructors

--- 05_clases.dart
+++ 05_clases.dart
@@ -1,19 +1,36 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 void main(List<String> args) {
-  final spiderman = Hero(name: "batman", power: "El dinero");
+  final Map<String, dynamic> rawJson = {
+    'name': 'Thor',
+    'power': 'Thunder',
+    'isAlive': true
+  };
 
-  print(spiderman);
-  print(spiderman.name);
-  print(spiderman.power);
+  final ironman = Hero(name: "Tony Stark", power: "El dinero", isAlive: false);
+  final thor = Hero.fromJson(rawJson);
+
+  print(ironman);
+  print(thor);
 }
 
 class Hero {
   String name;
   String power;
+  bool isAlive;
 
-  Hero({required this.name, required this.power});
+  Hero({
+    required this.name,
+    required this.power,
+    required this.isAlive,
+  });
+
+  Hero.fromJson(Map<String, dynamic> json)
+      : name = json['name'] ?? 'No name found',
+        power = json['power'] ?? 'No power found',
+        isAlive = json['isAlive'] ?? 'No isAliveFound';
 
   @override
   String toString() {
-    return '$name - $power';
+    return '$name - $power - Alive: ${isAlive ? 'YES!!!' : 'Ño'}';
   }
 }
