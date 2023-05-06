5287ad718c698974625403146efa884efee860e1
E1230 <embeltrans@correo.udistrital.edu.co>
2023-05-06 02:42:09 -0500

    streams

---
+++ 10_streams.dart
@@ -0,0 +1,11 @@
+void main(List<String> args) {
+  emitNumbers().listen((value) {
+    print('Stream value: $value');
+  });
+}
+
+Stream<int> emitNumbers() {
+  return Stream.periodic(const Duration(seconds: 1), (value) {
+    return value;
+  }).take(5);
+}
